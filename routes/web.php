604f2db1a76f478128a0537a2b1693bfc8f2c5ad
Junior-Shyko <franciscoanto@gmail.com>
2024-07-13 18:02:25 -0300

    :hammer: #3 - Instalado multi-tenancy
    Instalação do pacote

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::domain('localhost')->group(function () {
+    Route::get('/', function (string $tenant) {
+       return "Localhost";
+    });
+});
+
+Route::domain('{tenant}.localhost')->group(function () {
+    Route::get('/', function (string $tenant) {
+        Route::view('/', 'welcome');
+    });
+});
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
