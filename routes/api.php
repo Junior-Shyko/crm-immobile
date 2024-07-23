55bebb051664cb7a97db8145465fd4a573cc26c7
Junior-Shyko <franciscoanto@gmail.com>
2024-07-23 12:17:25 -0300

    Adicionado arquivo para rotas da api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-   return "api";
+Route::domain('{tenant}.localhost')->group(function () {
+    Route::get('/', function (string $tenant) {
+        return $tenant;
+    });
 });
+
 
