1c68eb25059f01cea12bb7e018aa83aa3721a56a
Junior-Shyko <franciscoanto@gmail.com>
2024-07-23 21:59:59 -0300

    :construction: #7 - Adicionado papeis na pagina
    Usando o componente livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'CRM') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+        @filamentStyles
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -31,6 +36,7 @@
             <main>
                 {{ $slot }}
             </main>
+            @filamentScripts
         </div>
     </body>
 </html>
